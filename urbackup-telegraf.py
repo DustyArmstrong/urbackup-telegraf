fcee06aabbeeafe5e4d989d07f8765825f91ed5c
DustyArmstrong <pseudokitten@gmail.com>
2023-09-21 13:53:02 +0100

    Update urbackup-telegraf.py

--- urbackup-telegraf.py
+++ urbackup-telegraf.py
@@ -9,8 +9,6 @@ server = urbackup_api.urbackup_server("http://[YOUR_IP]:55414/x", urbackusername
 tgClient = TelegrafClient(host='localhost', port=8092)
 clients = server.get_status()
 for client in clients:
-    #print(client['image_ok'])
-    #print(client['lastbackup_image'])
     tgClient.metric('Urbackup Status', client['image_ok'], tags={'Endpoint': client['name']})
     tgClient.metric('Urbackup Last', client['lastbackup_image'], tags={'Endpoint': client['name']})
     tgClient.metric('Urbackup Seen', client['lastseen'], tags={'Endpoint': client['name']})
